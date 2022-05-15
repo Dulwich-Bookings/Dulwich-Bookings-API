d4ae3cb68ff894981cbef8ab0e4b8308cc859cc8
ShenyiCui <shenyicui@outlook.com>
2022-05-15 21:27:14 +0800

    add ssl to database for production

--- config/config.js
+++ config/config.js
@@ -14,5 +14,11 @@ module.exports = {
   production: {
     url: DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
