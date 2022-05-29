952fcc251a0dc6b73cfd4f5fc2a0216b06067114
Jason Qiu <jason_qiu@hotmail.com>
2022-05-29 13:59:03 +0800

    remove forget password token secret from environment

--- src/consts/enviroment.ts
+++ src/consts/enviroment.ts
@@ -2,5 +2,4 @@ export default {
   sendGridAPI: process.env.SENDGRID_API_KEY as string,
   saltRounds: parseInt(process.env.SALT_ROUNDS as string) as number,
   jwtAccessTokenSecret: process.env.TOKEN_SECRET as string,
-  jwtForgotPasswordTokenSecret: process.env.FORGET_PASSWORD_TOKEN as string,
 };
