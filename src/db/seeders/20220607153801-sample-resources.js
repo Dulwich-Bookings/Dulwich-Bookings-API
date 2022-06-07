8f7f54f54857da323233ad7552437bb77afb2905
Jason Qiu <jason_qiu@hotmail.com>
2022-06-08 00:06:24 +0800

    add migration to create resource table and seeders

---
+++ src/db/seeders/20220607153801-sample-resources.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('resources', [
+      {
+        name: 'A113',
+        description: 'This is an intriguing room.',
+        accessRights: Sequelize.literal(
+          "ARRAY['Admin', 'Teacher']::\"enum_resources_accessRights\"[]"
+        ),
+        bookingRights: Sequelize.literal(
+          "ARRAY['Admin', 'Teacher']::\"enum_resources_bookingRights\"[]"
+        ),
+        inAdvance: 3,
+        isBookingDescriptionOptional: true,
+        schoolId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'B113',
+        description: 'This is a boring room.',
+        accessRights: Sequelize.literal(
+          "ARRAY['Admin', 'Teacher', 'Student']::\"enum_resources_accessRights\"[]"
+        ),
+        bookingRights: Sequelize.literal(
+          "ARRAY['Admin', 'Teacher', 'Student']::\"enum_resources_bookingRights\"[]"
+        ),
+        inAdvance: 4,
+        isBookingDescriptionOptional: false,
+        schoolId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async queryInterface => {
+    return await queryInterface.bulkDelete('resources', null, {});
+  },
+};
