7bacfdce138bd734c996cd21a65008cf634e03cd
ShenyiCui <shenyicui@outlook.com>
2022-07-19 02:30:43 +0800

    add TagMap Controller and Routes
    - Init within App.ts

---
+++ src/controllers/TagMapController.ts
@@ -0,0 +1,93 @@
+import {NextFunction, Request, Response} from 'express';
+import userFriendlyMessages from '../consts/userFriendlyMessages';
+import {TagMapCreationAttributes} from '../models/TagMap';
+import TagMapService from '../services/TagMapService';
+
+export default class TagMapController {
+  private tagMapService: TagMapService;
+  constructor(tagMapService: TagMapService) {
+    this.tagMapService = tagMapService;
+  }
+
+  async createOneTagMap(req: Request, res: Response, next: NextFunction) {
+    try {
+      const toCreate: TagMapCreationAttributes = {
+        ...req.body,
+      };
+      const createdTagMap = await this.tagMapService.createOneTagMap(toCreate);
+      res.status(201);
+      res.json({
+        message: userFriendlyMessages.success.createTagMap,
+        data: createdTagMap,
+      });
+    } catch (e) {
+      res.status(400);
+      res.json({message: userFriendlyMessages.failure.createTagMap});
+      next(e);
+    }
+  }
+
+  async getAllTagMaps(res: Response, next: NextFunction) {
+    try {
+      const tagMaps = (await this.tagMapService.getAllTagMaps()) || [];
+      res.json({
+        message: userFriendlyMessages.success.getAllTagMap,
+        data: tagMaps,
+      });
+    } catch (e) {
+      res.status(400);
+      res.json({message: userFriendlyMessages.failure.getAllTagMap});
+      next(e);
+    }
+  }
+
+  async getOneTagMapById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const tagMap = await this.tagMapService.getOneTagMapById(id);
+      res.json({
+        message: userFriendlyMessages.success.getOneTagMap,
+        data: tagMap,
+      });
+    } catch (e) {
+      res.status(400);
+      res.json({message: userFriendlyMessages.failure.getOneTagMap});
+      next(e);
+    }
+  }
+
+  async updateOneTagMapById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const oldTagMap = await this.tagMapService.getOneTagMapById(id);
+      const updatedAttributes = {
+        ...oldTagMap,
+        ...req.body,
+      };
+      const updatedTagMap = await this.tagMapService.updateOneTagMapById(
+        id,
+        updatedAttributes
+      );
+      res.json({
+        message: userFriendlyMessages.success.updateTagMap,
+        data: updatedTagMap,
+      });
+    } catch (e) {
+      res.status(400);
+      res.json({message: userFriendlyMessages.failure.updateTagMap});
+      next(e);
+    }
+  }
+
+  async deleteOneTagMapById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      await this.tagMapService.deleteOneTagMapById(id);
+      res.json({message: userFriendlyMessages.success.deleteTagMap});
+    } catch (e) {
+      res.status(400);
+      res.json({message: userFriendlyMessages.failure.deleteTagMap});
+      next(e);
+    }
+  }
+}
---
+++ src/routes/TagMapRoutes.ts
@@ -0,0 +1,50 @@
+import express, {Request, Response, NextFunction} from 'express';
+
+import TagMapController from '../controllers/TagMapController';
+import Container from '../utils/container';
+import AuthenticationMiddleware from '../middlewares/authentication';
+import roleValidator, {TEACHERS} from '../middlewares/authorization';
+
+export default () => {
+  const tagMapRouter = express.Router();
+  const tagMapController: TagMapController =
+    Container.getInstance().get('TagMapController');
+  const authenticationMiddleware: AuthenticationMiddleware =
+    Container.getInstance().get('AuthenticationMiddleware');
+
+  const auth = (req: Request, res: Response, next: NextFunction) =>
+    authenticationMiddleware.authentication(req, res, next);
+
+  tagMapRouter.post(
+    '/',
+    [auth, roleValidator(TEACHERS)],
+    tagMapController.createOneTagMap.bind(tagMapController)
+  );
+
+  tagMapRouter.get(
+    '/',
+    [auth],
+    (_: Request, res: Response, next: NextFunction) =>
+      tagMapController.getAllTagMaps(res, next)
+  );
+
+  tagMapRouter.get(
+    '/:id',
+    [auth],
+    tagMapController.getOneTagMapById.bind(tagMapController)
+  );
+
+  tagMapRouter.put(
+    '/:id',
+    [auth, roleValidator(TEACHERS)],
+    tagMapController.updateOneTagMapById.bind(tagMapController)
+  );
+
+  tagMapRouter.delete(
+    '/:id',
+    [auth, roleValidator(TEACHERS)],
+    tagMapController.deleteOneTagMapById.bind(tagMapController)
+  );
+
+  return tagMapRouter;
+};
