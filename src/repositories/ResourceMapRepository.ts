e2fc7dfcca2a73eaeeee4c5ad847eeb257e22d56
ShenyiCui <shenyicui@outlook.com>
2022-07-19 23:05:50 +0800

    add resource map repository and service

---
+++ src/repositories/ResourceMapRepository.ts
@@ -0,0 +1,11 @@
+import {Sequelize} from 'sequelize/types';
+
+import ResourceMap from '../models/ResourceMap';
+import {ModelStatic} from '../types';
+import BaseRepository from './BaseRepository';
+
+export default class BookmarkRepository extends BaseRepository {
+  constructor(db: Sequelize) {
+    super(db.models[ResourceMap.name] as ModelStatic);
+  }
+}
