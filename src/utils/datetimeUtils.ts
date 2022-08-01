4b9cb74e340da89af6f32e59f3a3e7c89f3fb5fc
ShenyiCui <shenyicui@outlook.com>
2022-08-01 12:55:12 +0800

    catch validation errors for date and link

--- src/utils/datetimeUtils.ts
+++ src/utils/datetimeUtils.ts
@@ -1,11 +1,17 @@
 import userFriendlyMessages from '../consts/userFriendlyMessages';
 
+export class InvalidUTCStringError extends Error {
+  constructor() {
+    super(userFriendlyMessages.failure.invalidUTCString);
+  }
+}
+
 export function validateUTCString(utc: string): void {
   // Regex expression to check if string is in ISO 8601 UTC format (eg. 2020-01-01T00:00:00Z)
   const isUTCString = new RegExp(
     '\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z'
   );
   if (!isUTCString.test(utc)) {
-    throw new Error(userFriendlyMessages.failure.invalidUTCString);
+    throw new InvalidUTCStringError();
   }
 }
