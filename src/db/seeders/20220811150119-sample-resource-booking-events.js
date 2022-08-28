cd7259e171f077af9ba35428cccf2a72b77c7d57
jasonqiu212 <jason_qiu@hotmail.com>
2022-08-28 17:08:42 +0800

    Update RRULE for resource booking events seed file

--- src/db/seeders/20220811150119-sample-resource-booking-events.js
+++ src/db/seeders/20220811150119-sample-resource-booking-events.js
@@ -23,7 +23,7 @@ module.exports = {
         resourceBookingId: 3,
         startDateTime: '2020-05-05T17:00:00.000Z',
         endDateTime: '2020-05-05T19:00:00.000Z',
-        RRULE: 'RRULE:FREQ=WEEKLY;INTERVAL=2;COUNT=3',
+        RRULE: 'DTSTART:20200505T170000Z\nRRULE:FREQ=WEEKLY;INTERVAL=2;COUNT=3',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -31,7 +31,8 @@ module.exports = {
         resourceBookingId: 4,
         startDateTime: '2020-05-06T07:00:00.000Z',
         endDateTime: '2020-05-06T09:00:00.000Z',
-        RRULE: 'RRULE:FREQ=WEEKLY;INTERVAL=1;UNTIL=20200520T160000Z',
+        RRULE:
+          'DTSTART:20200506T070000Z\nRRULE:FREQ=WEEKLY;INTERVAL=1;UNTIL=20200520T160000Z',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
